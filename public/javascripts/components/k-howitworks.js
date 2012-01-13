c8d97c6a1266e31b2e5d1745cca0b2d68745f44f
Reed Lauber <reedlauber@gmail.com>
2012-01-13 14:31:39 -0500

    Added How it works carousel.

--- public/javascripts/components/k-howitworks.js
+++ public/javascripts/components/k-howitworks.js
@@ -2,7 +2,7 @@
 	K.HowItWorks = function(options) {
 		var _c = K.Component({
 			id:'howitworks',
-			duration: 2800,
+			duration: 3200,
 			_times: 50
 		}, options);
 		
@@ -14,6 +14,28 @@
 			_previous = 0,
 			_times = 0;
 		
+		function _moveSlide(advance) {
+			if(_c.options.times && _c.options.times > 0 && _times > _c.options.times) {
+				clearInterval(_interval);
+				return;
+			}
+		
+			$slides.removeClass('slide-previous');
+			if(advance) {
+				$($slides[_previous]).addClass('slide-previous').removeClass('slide-active').css('left', 'auto').animate({ right:'100%' }, 'normal', function() {
+					$(this).css('right', 'auto');
+				});
+				$($slides[_current]).addClass('slide-active').css('left', '100%').animate({ left:0 }, 'normal');
+			} else {
+				$($slides[_previous]).addClass('slide-previous').removeClass('slide-active').css('left', 'auto').animate({ left:'100%' }, 'normal', function() {
+					$(this).css('left', 'auto');
+				});
+				$($slides[_current]).addClass('slide-active').css('right', '100%').animate({ right:0 }, 'normal');
+			}
+			$($indicators[_previous]).removeClass('slide-indicator-active');
+			$($indicators[_current]).addClass('slide-indicator-active');
+		}
+		
 		function _advance() {	
 			_previous = _current++;
 			if(_current > $slides.length - 1) {
@@ -21,18 +43,16 @@
 				_times++;
 			}
 
-			if(_c.options.times && _c.options.times > 0 && _times > _c.options.times) {
-				clearInterval(_interval);
-				return;
-			}
+			_moveSlide(true);
+		}
 		
-			$slides.removeClass('slide-previous');
-			$($slides[_previous]).addClass('slide-previous').removeClass('slide-active').css('left', 'auto').animate({ right:'100%' }, 'normal', function() {
-				$(this).css('right', 'auto');
-			});
-			$($slides[_current]).addClass('slide-active').css('left', '100%').animate({ left:0 }, 'normal');
-			$($indicators[_previous]).removeClass('slide-indicator-active');
-			$($indicators[_current]).addClass('slide-indicator-active');
+		function _stepBack() {
+			_previous = _current--;
+			if(_current < 0) {
+				_current = $slides.length - 1;
+			}
+			
+			_moveSlide();
 		}
 		
 		function _setupLoop() {
@@ -45,9 +65,17 @@
 			$('.slide-next').click(function() {
 				if(_interval) {
 					clearInterval(_interval);
-					_advance();
-					_setupLoop();
 				}
+				_advance();
+				_setupLoop();
+			});
+			
+			$('.slide-prev').click(function() {
+				if(_interval) {
+					clearInterval(_interval);
+				}
+				_stepBack();
+				_setupLoop();
 			});
 		}
 		
